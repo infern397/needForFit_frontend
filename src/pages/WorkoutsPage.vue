327f3deab10c698b75c727da3f6dca2a83c59322
infern <semenmochalov55@gmail.com>
2024-02-20 20:40:26 +0700

    Init comit

--- src/pages/WorkoutsPage.vue
+++ src/pages/WorkoutsPage.vue
@@ -5,7 +5,16 @@
           v-for="(workout, i) in workoutsList"
           class="mb-3 mt-3"
           @click="$router.push({name: 'workout', params: { id: workout.id } })"
-          :title="`Тренировка №${i + 1}`" :text="workout.created_at" variant="tonal" color="teal"></v-card>
+          :title="`Тренировка №${i + 1}`" :text="workout.created_at" variant="tonal" color="teal">
+        <v-btn
+            @click.stop="deleteWorkout(workout.id)"
+            class="mt-5 mr-5 v-btn--size-x-small"
+            position="absolute"
+            fab
+            icon="mdi-delete"
+            location="top right"
+        ></v-btn>
+      </v-card>
     </v-list>
   </v-container>
   <v-btn
@@ -50,6 +59,19 @@ export default {
       axios.post('http://127.0.0.1:8000/api/workouts/', {"uid": 1})
           .then(res => {
             this.workoutsList.push(res.data)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .finally(() => {
+            this.loading = false;
+          })
+    },
+    deleteWorkout(id) {
+      axios.delete('http://127.0.0.1:8000/api/workouts/' + id)
+          .then(res => {
+            const workoutIndex = this.workoutsList.findIndex(e => e.id === res.data.id);
+            this.workoutsList.splice(workoutIndex, 1)
           })
           .catch(err => {
             console.log(err)
